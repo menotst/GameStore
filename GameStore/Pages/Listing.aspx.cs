4538a0e7b10f5285d20a7094b3662f2fe8108757
rutara <rutara317@gmail.com>
2017-10-04 23:59:03 +0300

    постраничный вывод

--- GameStore/Pages/Listing.aspx.cs
+++ GameStore/Pages/Listing.aspx.cs
@@ -12,9 +12,22 @@ namespace GameStore.Pages
     public partial class Listing : System.Web.UI.Page
     {
         private Repository repository = new Repository();
+        private int pageSize = 4;
+
+        protected int CurrentPage
+        {
+            get
+            {
+                int page;
+                return int.TryParse(Request.QueryString["page"],out page) ?page:1 ;
+            }
+        }
         protected IEnumerable<Game> GetGames()
         {
-            return repository.Games;
+            return repository.Games
+                .OrderBy(g => g.GameId)
+                .Skip((CurrentPage - 1) * pageSize)
+                .Take(pageSize);
         }
         protected void Page_Load(object sender, EventArgs e)
         {
